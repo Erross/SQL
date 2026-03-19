e8a38f2314507753f0506fafde9fb2e06731947b
ewanr <ewanrross@yahoo.com>
2026-03-19 14:56:44 -0500

    nulls

--- TimeClashDiagnostic.sql
+++ TimeClashDiagnostic.sql
@@ -115,3 +115,50 @@ LEFT JOIN hub_owner.PEX_PROC_ELEM_EXEC pee
               SUBSTR(RAWTOHEX(pee.ID),21,12))
 WHERE s.SAMPLE_ID IN ('S003025','S002814','S003345')
 ORDER BY s.SAMPLE_ID;
+
+--find nulls
+SELECT 
+    s.SAMPLE_ID,
+    meas_s.ROW_INDEX,
+    pee.ID as PEE_ID,
+    peep.ID as PEEP_ID,
+    peep.SOURCE_POSITION,
+    (meas_s.ROW_INDEX - (
+        SELECT MIN(ms2.ROW_INDEX)
+        FROM hub_owner.RES_MEASUREMENTSAMPLE ms2
+        WHERE ms2.CONTEXT_ID = meas_s.CONTEXT_ID
+    )) as CALC_ITEM_INDEX,
+    pv.ITEM_INDEX as ACTUAL_ITEM_INDEX,
+    pv.VALUE_NUMERIC,
+    rt.TASK_ID,
+    rt.LIFE_CYCLE_STATE as TASK_STATE,
+    ms_master.SAMPLE_ID as MASTER_SAMPLE_ID,
+    coi.COLLABORATIVE_SPACE_ID as CS_ID
+FROM hub_owner.SAM_SAMPLE s
+JOIN hub_owner.RES_MEASUREMENTSAMPLE meas_s ON meas_s.MAPPED_SAMPLE_ID = s.ID
+JOIN hub_owner.RES_RETRIEVAL_CONTEXT ctx ON ctx.ID = meas_s.CONTEXT_ID
+JOIN hub_owner.PEX_PROC_ELEM_EXEC pee 
+     ON ctx.CONTEXT = 'urn:pexelement:' ||
+        LOWER(SUBSTR(RAWTOHEX(pee.ID),1,8)||'-'||SUBSTR(RAWTOHEX(pee.ID),9,4)||'-'||
+              SUBSTR(RAWTOHEX(pee.ID),13,4)||'-'||SUBSTR(RAWTOHEX(pee.ID),17,4)||'-'||
+              SUBSTR(RAWTOHEX(pee.ID),21,12))
+JOIN hub_owner.PEX_PROC_EXEC pe ON pe.ID = pee.PARENT_ID
+LEFT JOIN hub_owner.PEX_PROC_ELEM_EXEC_PARAM peep ON peep.PARENT_ID = pee.ID
+LEFT JOIN hub_owner.COR_PARAMETER_VALUE pv 
+     ON pv.PARENT_IDENTITY = peep.ID
+    AND pv.VALUE_NUMERIC IS NOT NULL
+    AND pv.ITEM_INDEX = (meas_s.ROW_INDEX - (
+        SELECT MIN(ms2.ROW_INDEX)
+        FROM hub_owner.RES_MEASUREMENTSAMPLE ms2
+        WHERE ms2.CONTEXT_ID = meas_s.CONTEXT_ID
+    ))
+LEFT JOIN hub_owner.REQ_TASK rt
+     ON rt.WORK_ITEM LIKE '%' || LOWER(
+            SUBSTR(RAWTOHEX(pe.ID),1,8)||'-'||SUBSTR(RAWTOHEX(pe.ID),9,4)||'-'||
+            SUBSTR(RAWTOHEX(pe.ID),13,4)||'-'||SUBSTR(RAWTOHEX(pe.ID),17,4)||'-'||
+            SUBSTR(RAWTOHEX(pe.ID),21,12)) || '%'
+    AND INSTR(','||rt.SAMPLE_LIST||',', ','||s.SAMPLE_ID||',') > 0
+LEFT JOIN hub_owner.SAM_SAMPLE ms_master ON s.MASTER_SAMPLE_ID = ms_master.ID
+LEFT JOIN hub_owner.COSPC_OBJECT_IDENTITY coi ON coi.OBJECT_ID = s.ID
+WHERE s.SAMPLE_ID IN ('S002814','S003025','S003345')
+ORDER BY s.SAMPLE_ID, peep.SOURCE_POSITION;
